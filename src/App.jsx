409e8c2e3953bb5d5b7623d4d25062e1c87ef244
rodri154321 <rodri154321@gmail.com>
2024-03-01 15:27:27 -0300

    Prueba visual

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='fondo1'>
+        <div>
+          <img className='image1' src="src\images\Logo2025.png" alt="Logo1" />
+        </div>
+        <h1>6 paises - 1 mision</h1>
+        <div className='banderas'>
+          <img className='bandera' src="https://i.postimg.cc/pTPwCN24/argentina.png" alt="Argentina" />
+          <img className='bandera' src="https://i.postimg.cc/3R9PbLNR/bolivia.png" alt="Bolivia" />
+          <img className='bandera' src="https://i.postimg.cc/J0n9wbHQ/chile.png" alt="Chile" />
+          <img className='bandera' src="https://i.postimg.cc/hv5NKf9R/paraguay.png" alt="Paraguay" />
+          <img className='bandera' src="https://i.postimg.cc/1RCkGYb7/peru.png" alt="Peru" />
+          <img className='bandera' src="https://i.postimg.cc/qMN9jxTJ/uruguay.png" alt="Uruguay" />
+        </div>
+        <div>
+          <h2>CIMA es un congreso de movilización y capacitación
+            misionera juvenil que se realiza hace 35 años
+            Consta de dos partes:
+            </h2>
+            <h1>
+            DESCUBRE + EXPERIMENTA = CIMA</h1>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className='fondo2'>
+        <h1>este fondo dos</h1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
