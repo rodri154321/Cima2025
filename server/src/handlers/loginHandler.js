6aa884e7ff33fe18558f12b6efd7eb973fa12491
rodri154321 <rodri154321@gmail.com>
2024-05-02 23:46:50 -0300

    controllers handlers y routes

--- server/src/handlers/loginHandler.js
+++ server/src/handlers/loginHandler.js
@@ -1,16 +1,43 @@
-const { postLogin } = require("../controllers/loginController");
+const { postLogin, addInfoUser, postRegisterUser} = require("../controllers/loginController");
 
 const loginUser = async (req, res) => {
-    const { email, password } = req.body;
-    try {
-      const response = await postLogin( email, password );
-      if(response) return res.status(200).json({ message: "Registro Exitoso", user: response, homeURL: "/" });
+  const { emailGoogle, password } = req.body;
+  try {
+    const response = await postLogin(emailGoogle, password);
+    if (response) return res.status(200).json({ message: "Ingreso Exitoso", user: response, homeURL: "/" });
 
-    } catch (error) {
-      return res.status(500).json({ error: error.message });
-    }
-  };
-
-  module.exports ={
-    loginUser
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
   }
+};
+
+const registerUser = async (req,res) =>{
+
+  const { emailGoogle, password } = req.body;
+  try {
+    const response = await postRegisterUser(emailGoogle, password);
+    if (response) return res.status(200).json({ message: "Registro Exitoso", user: response, homeURL: "/" });
+
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+
+};
+
+const infoUser = async (req,res) =>{
+
+  const {emailGoogle, nombre, apellido, telefono, documento, sexo, nacionalidad, ciudad, provincia, email, fechaNacimiento, estadoInscripcion, preinscripto, cartaPastoral, descubrePagos, experimentaPagos, esVoluntario, tieneMedicacion, detalleMedicacion, esAlergico, detalleAlergia, iglesia, nombrePastor, paisResidencia } = req.body;
+  try {
+    const response = await addInfoUser(emailGoogle, nombre, apellido, telefono, documento, sexo, nacionalidad, ciudad, provincia, email, fechaNacimiento, estadoInscripcion, preinscripto, cartaPastoral, descubrePagos, experimentaPagos, esVoluntario, tieneMedicacion, detalleMedicacion, esAlergico, detalleAlergia, iglesia, nombrePastor, paisResidencia);
+    if (response) return res.status(200).json({ message: "Actualizacion Exitosa", user: response, homeURL: "/" });
+
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+
+};
+
+
+module.exports = {
+  loginUser,registerUser,infoUser
+}
