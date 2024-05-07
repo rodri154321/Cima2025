546b199c4e94765cef97ce2a049f3cce5ad3f71f
rodri154321 <rodri154321@gmail.com>
2024-05-07 18:58:08 -0300

    port

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const { conn } = require("./src/db");
 const PORT = 3001;
            //alter
 conn.sync({ force: true}).then(() => {
-    server.listen(PORT, () => {
+    server.listen(PORT,"0.0.0.0", () => {
       console.log(`Server listening on port ${PORT}`,"0.0.0.0");
     })
     }).catch(error => console.error(error))
