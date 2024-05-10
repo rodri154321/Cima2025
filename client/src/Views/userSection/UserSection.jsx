0b480f4bf0d295bed83698880fd419777a9c65ce
rodri154321 <rodri154321@gmail.com>
2024-05-10 17:02:23 -0300

    cambios en userform

--- client/src/Views/userSection/UserSection.jsx
+++ client/src/Views/userSection/UserSection.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Login from '../../Component/Login'
+//import Login from '../../Component/Login'
 import RegistroFormulario from '../../Component/Form/Form'
 import style from "./UserSection.module.css"
 function UserSection() {
   return (
     <div className={style.fondolin}>
       <div className={style.contenedorViews}>
-      <Login/>
+      {/* <Login/> */}
         <RegistroFormulario/>
       </div>
     </div>
