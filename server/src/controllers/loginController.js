054d7d0b6066dcb00bce7f4ffedf5c6fcc294e1c
rodri154321 <rodri154321@gmail.com>
2024-05-13 14:58:49 -0300

    add info handler

--- server/src/controllers/loginController.js
+++ server/src/controllers/loginController.js
@@ -26,6 +26,7 @@ const postRegisterUser = async (emailGoogle, password) => {
 const addInfoUser = async (nombre,apellido,emailGoogle,email,fechaNacimiento,documento,sexo,nacionalidad,paisResidencia,provincia,ciudad,iglesia,pastor,telefonoPastor,esAlergico,detalleAlergia,tieneMedicacion,detalleMedicacion,telefono,telefonoEmergencia,nombreEmergencia,esDiabetico,esCeliaco,esVegetariano,detalleAlimentacion,participoCimaday,participoPrisma,participoEurovoluntariado,participoCima,participoSigue) => {
   const newUser = null
   const exist = await users.findOne({ where: { emailGoogle: emailGoogle } });
+  console.log(exist);
   if (!exist.preinscripto) {
     newUser = await users.update(
       { nombre: nombre },
