bc3003e185713422ae2e0ab3b11753ba1a286dba
rodri154321 <rodri154321@gmail.com>
2024-08-29 18:51:44 -0300

    mapa

--- client/src/Views/Landing/Landin.jsx
+++ client/src/Views/Landing/Landin.jsx
@@ -573,6 +573,12 @@ const Landin = () => {
           </a>
         </div>
 
+
+        <div className={style.maps}>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5080.267595775887!2d-64.25960239464817!3d-31.372632809543752!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94329ec56e325f1f%3A0xb4166b54d074b369!2sComplejo%20Ferial%20C%C3%B3rdoba!5e0!3m2!1ses-419!2sar!4v1724943301434!5m2!1ses-419!2sar" 
+          width="100%" height="100%"  allowfullscreen="true" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+
         <div className={style.footer}>
           <motion.img
             initial={{ scale: 0, opacity: 0 }}
