8966b6a167cb0ca7a6e642dfde86931991807630
Esteban Martin Pilchuman <estebanpilchuman02@gmail.com>
2024-09-11 22:40:19 -0300

    caracteres back cantidad

--- server/src/db.js
+++ server/src/db.js
@@ -3,7 +3,6 @@ const { Sequelize } = require("sequelize");
 require('dotenv/config')
 
 
-
 const fs = require('fs');
 const path = require('path');
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
